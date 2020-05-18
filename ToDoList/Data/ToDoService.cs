d9a7e28a9004bd472cadc48bf5f94a888ef9eaf5
HwanHo Lee <leehh1412@hotmail.com>
2020-05-18 15:59:05 +0900

    초기 커밋.

---
+++ ToDoList/Data/ToDoService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Context;
+using ToDoList.Models;
+
+namespace ToDoList.Data
+{
+    public class ToDoService
+    {
+        private TodoContext _todoContext;
+        public ToDoService(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+        public delegate void ToDoListHandler(Todo todo);//델리게이트를 이용하여 메시지를 전달할 통로를 만든다.
+        public static event ToDoListHandler Created; 
+        public static event ToDoListHandler Updated;
+
+        public static event ToDoListHandler Deleted;
+        public List<Todo> All()
+        {
+            return _todoContext.Todos.ToList();
+        }
+        public void Create(Todo todo)//생성, 메서드
+        {
+            _todoContext.Todos.Add(todo); //데이터베이스에 쓸 데이터를 준비한다.
+            _todoContext.SaveChanges(); //실제로 데이터를 데이터베이스에 작성한다.
+            Created?.Invoke(todo);//메시지가 널값일 경우 오류가 발생함으로 챗 딜리게이트를 실행할 때 널값의 경우도 포함시켜준다. 
+
+        }
+
+        public void Delete(Todo todo)//삭제, 메서드
+        {
+            _todoContext.Todos.Remove(todo);
+            _todoContext.SaveChanges();
+            Deleted?.Invoke(todo);//메시지가 널값일 경우 오류가 발생함으로 챗 딜리게이트를 실행할 때 널값의 경우도 포함시켜준다. 
+
+        }
+
+        public void Update(Todo todo)//업데이트, 메서드
+        {
+            _todoContext.Todos.Update(todo);
+            _todoContext.SaveChanges();
+            Updated?.Invoke(todo);//메시지가 널값일 경우 오류가 발생함으로 챗 딜리게이트를 실행할 때 널값의 경우도 포함시켜준다. 
+
+        }
+
+    }
+}
---
+++ ToDoList/Models/Todo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class Todo
+    {
+        public string Id { get; private set; } = Guid.NewGuid().ToString();
+
+        public string Title { get; set; }
+
+        public DateTime Date { get; set; }
+        public string Body { get; set; } = string.Empty;
+
+        public bool IsComplete { get; set; } = false;
+
+       
+    }
+}
